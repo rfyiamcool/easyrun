cbda2633ac5e7acdfaf9e0fa767330642c82a623
芮峰云 <rfyiamcool@163.com>
2015-09-08 08:32:32 +0800

    incr version to match pypi version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
         name = "easyrun",
-        version = "3.9.2",
+        version = "3.9.3",
         author = "ruifengyun",
         author_email = "rfyiamcool@163.com",
         description = "a simple subprocess manager,suport stream stdout \ async ",
